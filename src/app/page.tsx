1b0719fe8ea06345e1fbb10cf6215a17e4b7e787
kento suzuta <1207kent.0618.foad@gmail.com>
2026-02-28 00:32:58 +0900

    update: supabase実装に伴いモックデータ移動

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import ProductView from "@/features/product/components/ProductView";
+import { fetchProducts } from "@/lib/service/product";
 
-export default function Page() {
-  return <ProductView />;
+export default async function Page() {
+  const products = await fetchProducts();
+  return <ProductView products={products} />;
 }
--- src/features/product/components/ProductView.tsx
+++ src/features/product/components/ProductView.tsx
@@ -1,83 +1,19 @@
 "use client";
 
-import { Product } from "@/features/product/types/product";
+import type { Product } from "@/features/product/types/product";
 import { Box, Container, Grid } from "@mui/material";
 import { useProductCategoryTabHandler } from "../hooks/useProductCategoryTabHandler";
 import ProductCard from "./ProductCard";
 import ProductCategoryTab from "./ProductCategoryTab";
 import ProductTitle from "./ProductTitle";
 
-const mockProducts: Product[] = [
-  {
-    id: "1",
-    title: "プレミアムコットンTシャツ",
-    summary: "高品質な綿100%素材を使用した着心地抜群のTシャツ",
-    priceYen: 5800,
-    imageUrl:
-      "https://plus.unsplash.com/premium_photo-1755456918798-35ddc5afa423?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODl8fCVFMyU4MyU5NyVFMyU4MyVBQyVFMyU4MyU5RiVFMyU4MiVBMiVFMyU4MyVBMCVFMyU4MiVCMyVFMyU4MyU4MyVFMyU4MyU4OCVFMyU4MyVCM1QlRTMlODIlQjclRTMlODMlQTMlRTMlODMlODR8ZW58MHx8MHx8fDA%3D",
-    category: "ファッション",
-    inStock: true,
-  },
-
-  {
-    id: "2",
-    title: "スポーツスニーカー",
-    summary: "クッション性に優れたソールで長時間の歩行も快適",
-    priceYen: 12800,
-    imageUrl:
-      "https://images.unsplash.com/photo-1656164753657-8ff832063a71?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDd8fCVFMyU4MiVCOSVFMyU4MyU4QiVFMyU4MyVCQyVFMyU4MiVBQiVFMyU4MyVCQ3xlbnwwfHwwfHx8MA%3D%3D",
-    category: "シューズ",
-    inStock: true,
-  },
-  {
-    id: "3",
-    title: "ラグジュアリーウォッチ",
-    summary: "洗練されたデザインとスイス製ムーブメント搭載",
-    priceYen: 45000,
-    imageUrl:
-      "https://images.unsplash.com/photo-1628375162431-33c2e0a89670?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "アクセサリー",
-    inStock: true,
-  },
-  {
-    id: "4",
-    title: "デザイナーハンドバッグ",
-    summary: "本革を使用した上質なハンドバッグ。収納力も抜群",
-    priceYen: 28000,
-    imageUrl:
-      "https://images.unsplash.com/photo-1652427019217-3ded1a356f10?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fCVFMyU4MyU4RiVFMyU4MyVCMyVFMyU4MyU4OSVFMyU4MyU5MCVFMyU4MyU4MyVFMyU4MiVCMHxlbnwwfHwwfHx8MA%3D%3D",
-    category: "アクセサリー",
-    inStock: true,
-  },
-  {
-    id: "5",
-    title: "ワイヤレスヘッドホン",
-    summary: "ノイズキャンセリング機能搭載。高音質で音楽を楽しめる",
-    priceYen: 18900,
-    imageUrl:
-      "https://plus.unsplash.com/premium_photo-1679513691474-73102089c117?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8JUUzJTgzJUFGJUUzJTgyJUE0JUUzJTgzJUE0JUUzJTgzJUFDJUUzJTgyJUI5JUUzJTgzJTk4JUUzJTgzJTgzJUUzJTgzJTg5JUUzJTgzJTlCJUUzJTgzJUIzfGVufDB8fDB8fHww",
-    category: "電子機器",
-    inStock: true,
-  },
-  {
-    id: "6",
-    title: "UVカットサングラス",
-    summary: "UV400カット。偏光レンズで眩しさを軽減",
-    priceYen: 8500,
-    imageUrl:
-      "https://images.unsplash.com/photo-1577803645773-f96470509666?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    category: "ファッション",
-    inStock: true,
-  },
-];
-
-export default function ProductView() {
+export default function ProductView({ products }: { products: Product[] }) {
   const {
     categories,
     selectedCategory,
     setSelectedCategory,
     filteredProducts,
-  } = useProductCategoryTabHandler(mockProducts);
+  } = useProductCategoryTabHandler(products);
 
   return (
     <Container sx={{ py: 4 }}>
